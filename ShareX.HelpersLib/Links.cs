4da79f769daaff1f8fcae416b4d9b421eedde1cd
Cosebdd <Cosebdd@gmail.com>
2025-02-16 00:46:23 +0100

    Update manager repository, AppId and links. Excluded the uploaders project from the build. Fixed UploaderFactory stub

--- ShareX.HelpersLib/Links.cs
+++ ShareX.HelpersLib/Links.cs
@@ -28,7 +28,7 @@ namespace ShareX.HelpersLib
     public static class Links
     {
         public const string Website = "https://getsharex.com";
-        public const string GitHub = "https://github.com/ShareX/ShareX";
+        public const string GitHub = "https://github.com/Cosebdd/ShareNot";
         public const string GitHubIssues = GitHub + "/issues?q=is%3Aissue";
         public const string Callback = Website + "/callback/";
         public const string Changelog = Website + "/changelog";
